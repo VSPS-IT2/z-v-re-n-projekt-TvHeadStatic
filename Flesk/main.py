63cfb55415e4f60def86e6a6295b298f46102f0c
Static <angiebusinessacc@gmail.com>
2024-06-02 22:30:35 +0200

    noha krista

--- Flesk/main.py
+++ Flesk/main.py
@@ -2,13 +2,25 @@ from flask import *
 
 app = Flask(__name__)
 
+array_of_posts = [
+    ["Title", "Username", "Content"]
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html", title = "Blooger 1000")
+    print(str(array_of_posts))
+    return render_template("index.html", title = "Blooger 1000", current_posts = array_of_posts)
 
-@app.route("/post")
+@app.route("/post", methods=["GET", "POST"])
 def post():
-    return render_template("totalydone.html", title = "posting that 100% works")
+    global array_of_posts
+    match(request.method):
+        case "POST":
+            print(str(request.form))
+            array_of_posts = [[request.form["title"], request.form["author"], request.form["content"]]] + array_of_posts
+            return redirect("/", code=302)
+        case _:
+            return render_template("totalydone.html", title = "posting that 100% works")
 
 @app.errorhandler(404)
 def page_not_found(e):
